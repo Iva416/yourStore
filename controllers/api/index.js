aba796c9b6ae165fd259e1818d7cf43374612ed9
Jaymen L <jaymen100@gmail.com>
2022-10-27 13:26:39 -0400

    index fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const loginroutes = require('./login-routes');
+const useroutes = require('./user-routes');
 const cartroutes = require('./cart-routes');
 const orderroutes = require('./order-routes');
 const productroutes = require('./product-routes');
 
-router.use('/login', loginroutes);
+router.use('/user', useroutes);
 router.use('/cart', cartroutes);
 router.use('/orders', orderroutes);
 router.get('/products', productroutes);
