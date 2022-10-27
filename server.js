9fc22471d34ddd0c660e31af628d301bb989459b
Jaymen L <jaymen100@gmail.com>
2022-10-27 11:23:58 -0400

    Added user routes

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const path = require("path");
 const express = require("express");
+const session = require('express-session');
 const exphbs = require("express-handlebars");
 
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
