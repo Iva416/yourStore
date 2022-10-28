6dafb127bca4f83d02e2daa4629348d4bb6bd87f
Jaymen L <jaymen100@gmail.com>
2022-10-28 11:16:46 -0400

    Added order-routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 const api = require('./api');
 const home = require('./home-routes');
+const login = require('./login-routes');
 
+router.use('/login', login);
 router.use('/api', api);
 router.use('/', home);
 
