6f2a390ee64d0fea522f3e11dea9398ca30698ca
BenLiu <liumanpan1990@hotmail.com>
2022-10-28 20:12:44 -0400

    finish functionality of shopping cart

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -45,6 +45,7 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+      req.session.name = userData.name;
 
       res.json({ user: userData, message: 'You are now logged in!' });
     });
---
+++ public/js/cart.js
@@ -0,0 +1,50 @@
+const delButtonHandler = async (event) => {
+  event.preventDefault();
+
+  if (event.target.hasAttribute('data-pid')) {
+    const pid = event.target.getAttribute('data-pid');
+    const cid = event.target.getAttribute('data-cid');
+
+    const response = await fetch('/api/cart', {
+      method: 'DELETE',
+      body: JSON.stringify({ pid, cid }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      console.log('good');
+      document.location.replace('/cart');
+    } else {
+      alert('Failed to delete project');
+    }
+  }
+};
+
+const submitButtonHandler = async (event) => {
+  event.preventDefault();
+
+  if (event.target.hasAttribute('data-cart')) {
+    const cid = event.target.getAttribute('data-cart');
+
+    const response = await fetch(`/api/cart/submit/${cid}`, {
+      method: 'POST',
+    });
+
+    if (response.ok) {
+      //   console.log(await response.json());
+      document.location.replace('/cart');
+    } else {
+      alert('Shopping Cart is Empty~! Please add some products first.');
+    }
+  }
+};
+
+document
+  .querySelector('.product-list')
+  .addEventListener('click', delButtonHandler);
+
+document
+  .querySelector('#submit-cart')
+  .addEventListener('click', submitButtonHandler);
