16ddf0e983a53769c62ee25a984ce04f8f7b8a90
BenLiu <liumanpan1990@hotmail.com>
2022-10-30 15:16:58 -0400

    sign up page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,4 +39,12 @@ router.get('/login', async (req, res) => {
   }
 });
 
+router.get('/signup', async (req, res) => {
+  try {
+    res.render('signup');
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
